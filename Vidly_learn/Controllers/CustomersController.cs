1c8a717d8c8746c936618c05de1d19cef825db5e
Zhen <zhencao93@gmail.com>
2016-12-30 17:33:33 -0500

    edit customer

--- Vidly_learn/Controllers/CustomersController.cs
+++ Vidly_learn/Controllers/CustomersController.cs
@@ -9,53 +9,77 @@ using Vidly_learn.ViewModels;
 
 namespace Vidly_learn.Controllers
 {
-    public class CustomersController : Controller
-    {
-        private ApplicationDbContext _context;
+	public class CustomersController : Controller
+	{
+		private ApplicationDbContext _context;
 
-        public CustomersController()
-        {
-            _context = new ApplicationDbContext();
-        }
+		public CustomersController()
+		{
+			_context = new ApplicationDbContext();
+		}
 
-        protected override void Dispose(bool disposing)
-        {
-            _context.Dispose();
-        }
+		protected override void Dispose(bool disposing)
+		{
+			_context.Dispose();
+		}
 
-        public ActionResult New()
-        {
-            var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel
-            {
-                MembershipTypes = membershipTypes
-            };
-            return View(viewModel);
-        }
-        [HttpPost]
-        public ActionResult Create(Customer customer) //model binding
-        {
-            _context.Customers.Add(customer);
-            _context.SaveChanges();
-            return RedirectToAction("Index", "Customers");
-        }
+		public ActionResult New()
+		{
+			var membershipTypes = _context.MembershipTypes.ToList();
+			var viewModel = new CustomerFormViewModel
+			{
+				MembershipTypes = membershipTypes
+			};
+			return View("CustomerForm", viewModel);
+		}
+		[HttpPost]
+		public ActionResult Save(Customer customer) //model binding
+		{
+		    if (customer.Id == 0)
+		        _context.Customers.Add(customer);
+		    else
+		    {
+		        var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+		        customerInDb.Name = customer.Name;
+		        customerInDb.Birth = customer.Birth;
+		        customerInDb.MembershipTypeId = customer.MembershipTypeId;
+		        customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
 
-        // GET: Customers
-        public ActionResult Index()
-        {
-            var customers = _context.Customers.Include(c =>c.MembershipType).ToList();
-            return View(customers);
-        }
-        public ActionResult Details(int id)
-        {
-            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
-            if (customer == null)
-                return HttpNotFound();
+		    }
+			_context.SaveChanges();
+			return RedirectToAction("Index", "Customers");
+		}
 
-            return View(customer);
-        }
+		// GET: Customers
+		public ActionResult Index()
+		{
+			var customers = _context.Customers.Include(c =>c.MembershipType).ToList();
+			return View(customers);
+		}
+		public ActionResult Details(int id)
+		{
+			var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
+			if (customer == null)
+				return HttpNotFound();
+
+			return View(customer);
+		}
 
 
+		public ActionResult Edit(int id)
+		{
+			var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+			if (customer == null)
+			{
+				return HttpNotFound();
+			}
+			var viewModel = new CustomerFormViewModel
+			{
 
-    }
+				Customer = customer,
+				MembershipTypes = _context.MembershipTypes.ToList()
+			};
+			return View("CustomerForm", viewModel);
+		}
+	}
 }
--- Vidly_learn/ViewModels/CustomerFormViewModel.cs
+++ Vidly_learn/ViewModels/CustomerFormViewModel.cs
@@ -6,7 +6,7 @@ using Vidly_learn.Models;
 
 namespace Vidly_learn.ViewModels
 {
-    public class NewCustomerViewModel
+    public class CustomerFormViewModel
     {
         public IEnumerable<MembershipType> MembershipTypes { get; set; } //dont use list because we dont need to do add, edit operations to the membershiptype in here. We only need to iterate all membershiptypes. So IEnumerable is better to use
         public Customer Customer{ get; set; }
