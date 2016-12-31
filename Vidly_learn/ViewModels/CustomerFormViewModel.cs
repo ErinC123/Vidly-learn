5e05a6b1c36e14a368c4a997bea1274077dedf48
Zhen <zhencao93@gmail.com>
2016-12-30 19:05:47 -0500

    Add button and change title

--- Vidly_learn/ViewModels/CustomerFormViewModel.cs
+++ Vidly_learn/ViewModels/CustomerFormViewModel.cs
@@ -11,5 +11,17 @@ namespace Vidly_learn.ViewModels
         public IEnumerable<MembershipType> MembershipTypes { get; set; } //dont use list because we dont need to do add, edit operations to the membershiptype in here. We only need to iterate all membershiptypes. So IEnumerable is better to use
         public Customer Customer{ get; set; }
 
+        public string Title
+        {
+            get
+            {
+                if (Customer != null && Customer.Id != 0)
+                {
+                    return "Edit Customer";
+                }
+                return "Add Customer";
+            }
+        }
+
     }
 }
