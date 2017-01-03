96880c22ca9715a7d0f6467c7edd0a647e9e8c0b
Zhen <zhencao93@gmail.com>
2017-01-03 00:04:50 -0500

    Add Customer Dto and applied auto mapper

---
+++ Vidly_learn/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Vidly_learn.Dtos;
+using Vidly_learn.Models;
+
+namespace Vidly_learn.App_Start
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
