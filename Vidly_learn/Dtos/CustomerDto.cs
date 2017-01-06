b66457a5649f01a2270ffc042e8c2c81363480cd
Zhen <zhencao93@gmail.com>
2017-01-05 22:32:19 -0500

    hierachical data - customer's membershiptype

--- Vidly_learn/Dtos/CustomerDto.cs
+++ Vidly_learn/Dtos/CustomerDto.cs
@@ -17,6 +17,8 @@ namespace Vidly_learn.Dtos
 
         public bool IsSubscribedToNewsletter { get; set; }
 
+        public MembershipTypeDto MembershipType { get; set; }
+
         public byte MembershipTypeId { get; set; }
 
         //[Min18YearsIfAMember]
