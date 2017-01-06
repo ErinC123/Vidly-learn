4a5bd3a229749865d8e6ed4ffdb1847d74a56f94
Zhen <zhencao93@gmail.com>
2017-01-05 21:40:02 -0500

    Applied datatable plug in with ajax source

--- Vidly_learn/Controllers/CustomersController.cs
+++ Vidly_learn/Controllers/CustomersController.cs
@@ -64,8 +64,8 @@ namespace Vidly_learn.Controllers
 		// GET: Customers
 		public ActionResult Index()
 		{
-			var customers = _context.Customers.Include(c =>c.MembershipType).ToList();
-			return View(customers);
+			
+			return View();
 		}
 		public ActionResult Details(int id)
 		{
